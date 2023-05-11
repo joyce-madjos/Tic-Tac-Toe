525682e47955e495dbda78bed6d0f9f270366b18
joyce-madjos <madjos.joyce.arapan@gmail.com>
2023-05-11 16:39:30 +0800

    Added CSS reset

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let start, playerChooseX, gameStart, dialog, playerChooseO, choiceText, resultText, player;
-dialog = document.querySelector("dialog");
-start = document.getElementById("start");
-playerChooseX = document.getElementById("player-x");
-playerChooseO = document.getElementById("player-o");
-gameStart = document.getElementById("pop-start");
-choiceText = document.getElementById("choice-text");
-resultText = document.getElementById("result");
-
+let player;
+let dialog = document.querySelector("dialog");
+let start = document.getElementById("start");
+let playerChooseX = document.getElementById("player-x");
+let playerChooseO = document.getElementById("player-o");
+let gameStart = document.getElementById("pop-start");
+let choiceText = document.getElementById("choice-text");
+let resultText = document.getElementById("result");
+let restartGame = document.getElementById("repeat-game");
 
 start.addEventListener("click", function () {
   dialog.showModal();
@@ -17,43 +17,83 @@ playerChooseX.addEventListener("click", function () {
   dialog.close();
   choiceText.style.display = "block";
   player = "X";
+  choiceText.innerHTML = "Your choice: " + player + "";
 });
 
 playerChooseO.addEventListener("click", function () {
   dialog.close();
   choiceText.style.display = "block";
-
+  choiceText.innerHTML = "Your choice: " + player + "";
   player = "O";
 });
 
-// Create an array with 9 elements and initialize each element to 0
-var elementsValue = Array(9).fill(0);
+restartGame.addEventListener("click", function () {
+  window.location.reload();
+});
 
-var elements = [];
+// Create an array with 9 elements and initialize each element to 0
+let elementsValue = Array(9).fill(0);
+// Create an array to store the elements
+let elements = [];
+
+let i;
 
 // Loop through IDs "1" to "9"
-for (var i = 1; i <= 9; i++) {
+for (i = 1; i <= 9; i++) {
   // Generate the ID value
   var id = i.toString();
 
   // Select the element with the generated ID value
   var element = document.getElementById(id);
+
+  // Add the element to the array
   elements.push(element);
+
   // Add the onclick event listener to the element using a closure
   (function (id) {
     element.addEventListener("click", function () {
-      index = id - 1;
+      let index = id - 1;
+
       // Display player's move
       elements[index].innerHTML = player;
+
+      elements[index].addEventListener("click", (e) => {
+        e.preventDefault();
+      });
+
       checkWinner();
+      let computerChoice = checkAvailability();
+
       // Change the value of the element in the array to 1
       elementsValue[index] = "1";
-      // Change the text of the choiceText
-      choiceText.innerHTML = "Computer's turn";
+
+      let computer;
+      if (player === "X") {
+        computer = "O";
+      } else if (player === "O") {
+        computer = "X";
+      }
+
+      elements[computerChoice].innerHTML = computer;
     });
   })(id);
 }
 
+function checkAvailability() {
+  const elements = document.querySelectorAll(".box");
+  const emptyElements = [];
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+    if (element.textContent.trim().length === 0) {
+      emptyElements.push(i);
+    }
+  }
+  const randomIndex =
+    emptyElements[Math.floor(Math.random() * emptyElements.length)];
+  console.log(`Random index: ${randomIndex + 1}`);
+
+  return randomIndex;
+}
 
 function sum() {
   let elementsValueNumber = elementsValue.map(Number);
@@ -77,7 +117,6 @@ function sum() {
   };
 }
 
-
 function checkWinner() {
   let result = new sum();
 
@@ -93,33 +132,26 @@ function checkWinner() {
   ) {
     resultText.style.display = "block";
   }
-
 }
 
+// function getComputerChoice() {
+//   let moves = ["rock", "scissor", "paper"];
+//   let randomChoice = Math.floor(Math.random() * 3);
+//   let computerChoice = moves[randomChoice];
 
+//   if (computerChoice === "rock") {
+//     computerRockImage.style.backgroundColor = "wheat";
+//     textResultComputer.innerHTML = "ROCK";
+//   } else if (computerChoice === "paper") {
+//     computerPaperImage.style.backgroundColor = "wheat";
+//     textResultComputer.innerHTML = "PAPER";
+//   } else if (computerChoice === "scissor") {
+//     computerScissorImage.style.backgroundColor = "wheat";
+//     textResultComputer.innerHTML = "SCISSOR";
+//   }
 
-function getComputerChoice() {
-  let moves = ["rock", "scissor", "paper"];
-  let randomChoice = Math.floor(Math.random() * 3);
-  let computerChoice = moves[randomChoice];
-
-  if(computerChoice === "rock"){
-    computerRockImage.style.backgroundColor = "wheat";
-    textResultComputer.innerHTML = "ROCK";
-  }
-  else if(computerChoice === "paper"){
-    computerPaperImage.style.backgroundColor = "wheat";
-    textResultComputer.innerHTML = "PAPER";
-  }
-  else if(computerChoice === "scissor"){
-    computerScissorImage.style.backgroundColor = "wheat";
-    textResultComputer.innerHTML = "SCISSOR";
-  }
-
-  return computerChoice;
-}
-
-
+//   return computerChoice;
+// }
 
 // Close dialog on click outside
 // dialog.addEventListener("click", e => {
